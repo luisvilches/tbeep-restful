a2f7e38edc405f2b9ea464021c47c819516c292a
luis vilches <lvilches21@gmail.com>
2016-07-19 16:34:59 -0400

    api restful

---
+++ index.js
@@ -0,0 +1,91 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+var app = express();
+var Schema = mongoose.Schema;
+var ruta = "/api/puntos";
+
+//////////////////////////////////////////////////////////////////////
+/////////////// CONECCION A LA BASE DE DATOS /////////////////////////
+//////////////////////////////////////////////////////////////////////
+
+mongoose.connect('mongodb://luisvilches:andres2230@ds023475.mlab.com:23475/tbeep', function(err,res){
+	if (err) {
+		console.log("problemas al conectar con la base de datos" + err)
+	};
+});
+
+///////////////////////////////////////////////////////////////////////
+////////////////// CONFIGURACIONES ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: true}));
+
+
+///////////////////////////////////////////////////////////////////////
+/////////////////// SCHEMA DEL MODELO /////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+var beep = new Schema({
+
+	nombre: String,
+	direccion: String,
+	comuna: String,
+	x: String,
+	y: String
+
+});
+var beep = mongoose.model('beep',beep);
+
+///////////////////////////////////////////////////////////////////////
+/////////////////// RUTAS /////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+// GET
+app.get(ruta, function(req,res,next){
+	beep.find(function(err,data){
+		if (err) {
+			console.log(err);
+		};
+		res.status(200).jsonp({punto: data});
+	});
+});
+
+//GET:ID
+app.get(ruta+"/:id",function(req,res,next){
+
+	var id = req.params.id
+
+	beep.findOne({_id: id}, function(err,data){
+		if (err) {
+			console.log(err);
+		};
+		res.status(200).jsonp(data);
+	});
+});
+
+//POST
+app.post(ruta,function(req,res,next){
+
+	var data = new beep(req.body);
+	data.save(function(err){
+		if (err) {console.log(err)};
+		res.status(200).jsonp(data);
+	});
+});
+
+
+//PUT
+
+//DELETE
+
+///////////////////////////////////////////////////////////////////////
+////////////////// PUERTO DEL SERVIDOR ////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+app.listen(app.get('port'), function () {
+	console.log("App started at http://localhost:" + app.get('port'));
+});
